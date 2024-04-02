7bfb3cf175037a3881205b532cb4fb9042c92bce
Tarun018 <tarunkumarpoondamalli@gmail.com>
2024-04-02 11:17:42 +0530

    DONE

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package Controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
+import Model.Message;
+import Service.AccountService;
+import Service.MessageService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -9,24 +19,138 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
-    /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
-     * suite must receive a Javalin object from this method.
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
-     */
+    AccountService accountService;
+    MessageService messageService;
+
+    public SocialMediaController(){
+        this.accountService = new AccountService();
+        this.messageService = new MessageService();
+    }
+
+   
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
-
+        app.post("/register", this::postRegisterHandler);
+        app.post("/login", this::postLoginHandler);
+        app.post("/messages", this::postMessageHandler);
+        app.get("/messages", this::getAllMessagesHandler);
+        app.get("/messages/{message_id}", this::getMessageByIdHandler);
+        app.delete("/messages/{message_id}", this::deleteMessageByIdHandler);
+        app.patch("/messages/{message_id}", this::updateMessageHandler);
+        app.get("/accounts/{account_id}/messages", this::getMessagesHandler);
         return app;
     }
 
-    /**
-     * This is an example handler for an example endpoint.
-     * @param context The Javalin Context object manages information about both the HTTP request and response.
-     */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+    
+    private void postRegisterHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        try{
+            Account registerAccount = accountService.addAccount(account);
+            if(registerAccount!=null){
+                ctx.json(mapper.writeValueAsString(registerAccount));
+                ctx.status(200);
+            }else{
+                ctx.status(400);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            ctx.status(400);
+        
+        }
+        
+    }
+
+    private void postLoginHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account loginAccount = accountService.login(account);
+        if(loginAccount!=null){
+            ctx.json(mapper.writeValueAsString(loginAccount));
+            ctx.status(200);
+        }else{
+            ctx.status(401);
+        }
+    }
+
+    private void postMessageHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Message message = mapper.readValue(ctx.body(), Message.class);
+        Message createMessage = MessageService.createMessage(message);
+        if(createMessage!=null){
+            ctx.json(mapper.writeValueAsString(createMessage));
+            ctx.status(200);
+        }else{
+            ctx.status(400);
+        }
+
+    }
+
+    private void getAllMessagesHandler(Context ctx) throws JsonProcessingException {
+        List<Message> messages = messageService.getAllMessages(null);
+        ctx.json(messages);
+        ctx.status(200);
+    }
+
+    private void getMessageByIdHandler(Context ctx) throws JsonProcessingException{
+        System.out.println("!");
+        int message_id = Integer.parseInt(ctx.pathParam("message_id"));
+        if(messageService.getMessageById(message_id) != null){
+            try{
+                Message message = messageService.getMessageById(message_id);
+                ctx.json(message);
+                ctx.status(200);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+       
+    }
+
+    private void deleteMessageByIdHandler(Context ctx) throws JsonProcessingException{
+        int message_id = Integer.parseInt(ctx.pathParam("message_id"));
+        if(messageService.getMessageById(message_id) != null){
+            try{
+         Message deletedMessage = MessageService.deleteMessageById(message_id);
+            ctx.json(deletedMessage);
+            ctx.status(200);
+            }catch(Exception e){
+                e.printStackTrace();
+                ctx.status(200);
+            }
+        }else{
+            ctx.status(200);
+        }
+    }
+
+    private void updateMessageHandler(Context ctx) throws JsonProcessingException{
+        int message_id = Integer.parseInt(ctx.pathParam("message_id"));
+        ObjectMapper mapper = new ObjectMapper();
+        Message message = mapper.readValue(ctx.body(), Message.class);
+        //String body = ctx.body();
+        String newMessageText = message.message_text;
+        if(newMessageText.length() > 0 && newMessageText.length() <= 255){
+        
+        try {
+            MessageService.updateMessage(message_id, newMessageText);
+            ctx.status(400);
+            ctx.json(MessageService.getMessage(message_id));
+            ctx.status(400);
+            ctx.status(200);
+        } catch (Exception e) {
+            ctx.status(400);
+        }
+    }else{
+        ctx.status(400);
+    }
+    }
+
+    private void getMessagesHandler(Context ctx) throws JsonProcessingException, SQLException{
+        int account_id = Integer.parseInt(ctx.pathParam("account_id"));
+        List<Message> messages = messageService.getMessagesByAccountId(account_id);
+        ctx.json(messages);
+        ctx.status(200);
+
     }
 
 
---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,77 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class AccountDAO {
+    // for register of new account
+    public Account registerAccount(Account account) {
+        if(account.password.length() >= 4 && account.username.length() > 0){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            //SQL logic to insert 
+            String sql = "INSERT INTO account(username, password) VALUES (?,?);";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            //preparedStatement's setString and setInt methods
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+
+            preparedStatement.executeUpdate();
+            ResultSet pKeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pKeyResultSet.next()){
+                int generated_account_id = (int) pKeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+
+        }catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+        return null;
+    } 
+
+
+    // method to login to account
+    public Account login(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            // SQL logic
+            String sql = "SELECT * FROM account WHERE username = ? AND password = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            //preparedStatement's setString and setInt methods
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Validate user, return null
+        if (!rs.next()) {
+            return null;
+        }
+        // Extract the user information from the result set
+        int accountId = rs.getInt("account_id");
+        String username = rs.getString("username");
+        String password = rs.getString("password");
+        System.out.println(accountId);
+        System.out.println(username);
+
+        // Return the user information
+        return new Account(accountId, username,  password);
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+}
